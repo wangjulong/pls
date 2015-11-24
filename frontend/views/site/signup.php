4979a2ba7a7d9b37d3afdadc29f3f7bdd5371717
wangjulong <496349619@qq.com>
2015-11-24 15:43:16 +0800

    a little bug of signup.php
    1 a little bug of signup.php 书中的提示
    2 数据库备份

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -17,7 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+            <?php $form = ActiveForm::begin(['id' => 'signup-form']); ?>
 
                 <?= $form->field($model, 'username') ?>
 
